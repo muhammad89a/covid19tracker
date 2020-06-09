7a0da971958ef8467b5f0075d19d38d8a853e440
Muhammad Mahamid <mohammed.89a@gmail.com>
2020-06-09 17:25:01 +0300

    Updates

---
+++ precache-manifest.19e007bdf047ba502edb0b0395875785.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "407f5b0f4ff643d9dd5b57985ca03416",
+    "url": "/muhammad89a/covid19tracker/index.html"
+  },
+  {
+    "revision": "13a5121d09043e31f695",
+    "url": "/muhammad89a/covid19tracker/static/css/main.3ada6f44.chunk.css"
+  },
+  {
+    "revision": "d0b4f289e8359d61c6cd",
+    "url": "/muhammad89a/covid19tracker/static/js/2.7ca0d2a2.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/muhammad89a/covid19tracker/static/js/2.7ca0d2a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13a5121d09043e31f695",
+    "url": "/muhammad89a/covid19tracker/static/js/main.dd90caa2.chunk.js"
+  },
+  {
+    "revision": "4321a2f8ec245d59fa6f",
+    "url": "/muhammad89a/covid19tracker/static/js/runtime-main.ef172258.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/muhammad89a/covid19tracker/static/media/image.d7265326.png"
+  },
+  {
+    "revision": "63caf4f8cfa708d49aa41de0243440d5",
+    "url": "/muhammad89a/covid19tracker/static/media/stay_home_bg.63caf4f8.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/muhammad89a/covid19tracker/precache-manifest.19e007bdf047ba502edb0b0395875785.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/muhammad89a/covid19tracker/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
